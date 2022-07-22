a6e16e165cc7fdce48171f6be45b69cd35cccca4
Mariane Algayer <marianealgayer@gmail.com>
2022-07-22 12:25:16 -0300

    feat(routes): share negotiation route
    Set share negotiation page route (/acoes/negociar)

--- front-end/src/AppRoutes.tsx
+++ front-end/src/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import { Login } from './pages/Login';
 import { Shares } from './pages/Shares';
+import { ShareNegotiation } from './pages/ShareNegotiation';
 
 import { DefaultLayout } from './layouts/DefaultLayout';
 
@@ -12,6 +13,7 @@ export function AppRoutes() {
 
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/acoes" element={<Shares />} />
+        <Route path="/acoes/negociar" element={<ShareNegotiation />} />
       </Route>
     </Routes>
   );
