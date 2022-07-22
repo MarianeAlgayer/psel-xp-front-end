381d4531d6af37a58c36db2a494730019e4fda07
Mariane Algayer <marianealgayer@gmail.com>
2022-07-22 12:13:54 -0300

    feat(redux/userSlice): set buyShare action

--- front-end/src/redux/slices/userSlice.ts
+++ front-end/src/redux/slices/userSlice.ts
@@ -1,16 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 
-import { IShare } from '../../@types/share.d';
+import { IShare } from '../../@types/interfaces.d';
 
 interface IUserState {
-  email: string;
-  investments: {
-    shares: IShare[];
-  }
-  account: {
-    balance: number;
-  }
+ email: string;
+ investments: {
+   shares: IShare[];
+ }
+ account: {
+   balance: number;
+ }
 }
 
 const initialState: IUserState = {
@@ -30,9 +30,29 @@ export const userSlice = createSlice({
     saveEmail: (state, action: PayloadAction<string>) => {
       state.email = action.payload;
     },
+    buyShare: (state, action: PayloadAction<IShare>) => {
+      const foundShare = state.investments.shares
+        .find(({ code }) => action.payload.code === code);
+
+      if (!foundShare) {
+        state.investments.shares.push(action.payload);
+      } else {
+        state.investments.shares = state.investments.shares.map((share) => {
+          if (share.code === action.payload.code) {
+            return {
+              code: share.code,
+              qtd: (share.qtd + action.payload.qtd),
+              value: share.value,
+            };
+          }
+
+          return share;
+        });
+      }
+    },
   },
 });
 
-export const { saveEmail } = userSlice.actions;
+export const { saveEmail, buyShare } = userSlice.actions;
 
 export default userSlice.reducer;
