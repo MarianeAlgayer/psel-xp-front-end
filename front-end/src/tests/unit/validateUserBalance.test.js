eb44255a8a4ba1c3ebd591caf241bc022af50e22
Mariane Algayer <marianealgayer@gmail.com>
2022-07-24 16:49:49 -0300

    refactor: correct typo

--- front-end/src/tests/unit/validateUserBalance.test.js
+++ front-end/src/tests/unit/validateUserBalance.test.js
@@ -1,7 +1,7 @@
 import { validateUserBalance } from '../../helpers/validations';
 
-describe('Share quantity validation', () => {
-  describe('The buy/sell operation', () => {
+describe('User balance validation', () => {
+  describe('The deposit/withdraw operation', () => {
     it('The user balance should be greater than the operation value', () => {
       expect(validateUserBalance(1000, 2000)).toEqual(true);
       expect(validateUserBalance(1000, 500)).toEqual(false);
